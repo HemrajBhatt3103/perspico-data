4424a72cc4c1067ec72248399ba0968fc6092c37
lazlle-developer <hemrajb0990@gmail.com>
2025-12-12 23:00:40 +0530

    Feat:Node and npm management

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    turbopack: {},
   typescript: {
     ignoreBuildErrors: true,
   },
-
   reactStrictMode: false,
 
-  // keep your custom webpack config
-  webpack: (config, { dev }) => {
-    if (dev) {
+  
+  
+
+
+  webpack: (config, ctx) => {
+    if (ctx.dev) {
       config.watchOptions = {
         ignored: ["**/*"],
       };
@@ -19,3 +22,26 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+
+//   reactStrictMode: false,
+
+//   // keep your custom webpack config
+//   webpack: (config, { dev }) => {
+//     if (dev) {
+//       config.watchOptions = {
+//         ignored: ["**/*"],
+//       };
+//     }
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
